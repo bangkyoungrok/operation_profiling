cfd681a61b5d559115247be25547e3027afda7fc
bangkyoungrok <bkr0314@naver.com>
2022-05-17 12:16:43 +0900

    commit

--- get_diff_results.py
+++ get_diff_results.py
@@ -1,5 +1,6 @@
 import pickle
 from pprint import pprint
+from socket import VM_SOCKETS_INVALID_VERSION
 
 import pandas as pd
 
@@ -12,17 +13,18 @@ def get_diff_norm(x, y):
     diff_mean = torch.abs(x-y)
     diff_mean = torch.mean(diff_mean)
     base_mean = torch.mean(y)
+    mean = torch.mean(x)
     diff_norm_L1 = torch.norm(diff, p=1)
     diff_norm_L2 = torch.norm(diff, p=2)
     base_norm_L1 = torch.norm(y.mean(dim=0), p=1)
     base_norm_L2 = torch.norm(y.mean(dim=0), p=2)
-    return diff_mean.item(), diff_mean.div_(base_mean).item(), diff_norm_L1.item(), diff_norm_L2.item(), diff_norm_L1.div_(base_norm_L1).item(), diff_norm_L2.div_(base_norm_L2).item()
+    return mean.item(), diff_mean.item(), diff_mean.div_(base_mean).item(), diff_norm_L1.item(), diff_norm_L2.item(), diff_norm_L1.div_(base_norm_L1).item(), diff_norm_L2.div_(base_norm_L2).item()
 
 
 if __name__ == '__main__':
 
     device_list = ['gpu', 'tpu']
-    op_list = ['softmax', 'GeLU', 'tanh', 'sigmoid', 'swish']
+    op_list = ['exp', 'softmax', 'GeLU', 'tanh', 'sigmoid', 'swish']
     op_result = {}
 
     # loading previous results
@@ -38,7 +40,9 @@ if __name__ == '__main__':
     for op_name, op_val in op_result.items(): # 이 밑에는 device별로 있음
         
         for device, op_result_tensor in op_val.items():
-            diff_mean, base_ratio, diff_norm_L1, diff_norm_L2, diff_ratio_L1, diff_ratio_L2 = get_diff_norm(op_result_tensor, op_val['gpu'])
+            mean, diff_mean, base_ratio, diff_norm_L1, diff_norm_L2, diff_ratio_L1, diff_ratio_L2 = get_diff_norm(op_result_tensor, op_val['gpu'])
+            print("*"*20)
+            print(f"op_name : {op_name}, device : {device},mean : {mean}")
             item = {
                 'op': op_name,
                 'device': device,
@@ -47,8 +51,10 @@ if __name__ == '__main__':
                 'diff_norm_L1': diff_norm_L1,
                 'diff_norm_L2': diff_norm_L2,
                 'diff_ratio_L1': diff_ratio_L1,
-                'diff_ratio_L2': diff_ratio_L2
+                'diff_ratio_L2': diff_ratio_L2,
+                'mean' : mean
             }
+            
             diff_result.append(item)
 
 
@@ -79,6 +85,10 @@ if __name__ == '__main__':
     print("\n\n\nDIFF_RAIO_L2\n")
     pprint(df_diff_ratio_L2) 
     
+    df_mean = pd.pivot(result_pd, index='op', columns='device', values='mean')
+    print("\n\nMEAN\n")
+    pprint(df_mean)
+    
     df_diff_mean.to_csv('./result/result_diff_mean.csv')
     df_base_ratio.to_csv('./result/resut_diff_ratio.csv')
     result_pd.to_csv('./result/result_table.csv')
@@ -86,4 +96,4 @@ if __name__ == '__main__':
     df_diff_norm_L2.to_csv('./result/result_norm_L2_table.csv')
     df_diff_ratio_L1.to_csv('./result/reslut_ratio_L1_table.csv')
     df_diff_ratio_L2.to_csv('./result/reslut_ratio_L2_table.csv')
-    
+    df_mean.to_csv('./result/mean.csv')
--- tensors/test.py
+++ tensors/test.py
@@ -1,11 +1,38 @@
+import torch 
 import pickle
+import pandas as pd
+from pprint import pprint
 
-with open("/home/rudfhr0314/HYU/BDSL/operation_profiling/tensors/gpu_output/gpu_torch_softmax_output.pickle", "rb") as fr:
-    input = pickle.load(fr)
-    
+device_list = ['gpu', 'tpu']
+op_list = ['exp', 'softmax', 'GeLU', 'tanh', 'sigmoid', 'swish']
+result = {}
+for op in op_list:
+    result[op] = {}
+    for device in device_list:
+        with open(f"./tensors/{device}_output/{device}_torch_{op}_output.pickle", "rb") as fr:
+            result[op][device] = pickle.load(fr)
+        
+mean_result = []
+
+def re_mean(x):
+    mean = torch.mean(x)
+    return mean.item()
+
+for op_name, op_val in result.items():
+    for device, op_result in op_val.items():
+        mean = re_mean(op_result)
+        item = {
+            'op' : op_name,
+            'device' : device,
+            'mean' : mean
+        }
+        mean_result.append(item)
+    """
+    print(op_name, op_val)
+    break
+    """
+result_pd = pd.DataFrame(mean_result)
+pprint(result_pd)
 
 
-with open("/home/rudfhr0314/HYU/BDSL/operation_profiling/tensors/input_tensor.pickle", "rb") as fr:
-    input = pickle.load(fr)
-    
-print(input)
+result_pd.to_csv('./tensors/mean.csv')
