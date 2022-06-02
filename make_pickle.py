7106ec18e0762eeafbf53ce78a207ebe2c26061d
bangkyoungrok <bkr0314@naver.com>
2022-06-02 18:38:05 +0900

    commit

--- make_pickle.py
+++ make_pickle.py
@@ -1,12 +1,20 @@
-import pickle
 import numpy as np
+import pickle
 
-#from tensorflow.python.client import device_lib
-#print(device_lib.list_local_devices())
 
-np.random.seed(0)
-input = np.random.rand(100, 256, 256)
-# print(f"seed(0)_input : {input}") # seed(0)_input : [[0.5488135  0.71518937]]
 
-with open("data.pickle", "wb") as fw:
-    pickle.dump(input, fw)
+tensor_10 = np.random.uniform(-10,10,(100,256,256))
+tensor_1 = np.random.uniform(-1,1,(100,256,256))
+tensor_10_1 = np.random.uniform(-10**(-1),10**(-1),(100,256,256))
+tensor_10_2 = np.random.uniform(-10**(-2),10**(-2),(100,256,256))
+tensor_10_3 = np.random.uniform(-10**(-3),10**(-3),(100,256,256))
+tensor_posit10 = np.random.uniform(0,10,(100,256,256))
+tensor_negat10 = np.random.uniform(-10,0,(100,256,256))
+tensors_range = ['input_tensor_10', 'input_tensor_posit10', 'input_tensor_negat10', 'input_tensor_1', 'input_tensor_10(-1)','input_tensor_10(-2)', 'input_tensor_10(-3)']
+
+tensors = [tensor_10, tensor_posit10, tensor_negat10, tensor_1, tensor_10_1, tensor_10_2, tensor_10_3]
+
+for ranges, tensor in zip(tensors_range, tensors):
+    print(ranges)
+    with open(f"/home/rudfhr0314/HYU/BDSL/operation_profiling/tensors/{ranges}.pickle", "wb") as fw:
+        pickle.dump(tensor, fw)
--- models/torch_gpu.py
+++ models/torch_gpu.py
@@ -1,9 +1,11 @@
 import pickle
 import torch.nn as nn
 import torch
+import numpy as np
 
-with open("../tensors/input_tensor.pickle", "rb") as fr:
+with open("./tensors/input_tensor_negat10.pickle", "rb") as fr:
     input = pickle.load(fr)
+    #input = np.round(input, 2)
 
 #-------------------------------------------------------------------------------------------------
 
@@ -14,60 +16,46 @@ torch_input = torch.Tensor(input)
 torch_input = torch_input.to(device)
 #print(f"tensor_input : {torch_input}") 
 
-# exp
 
-torch_exp_output = torch.exp(torch_input)
-torch_exp_output = torch_exp_output.to('cpu')
-print(f"torch_softmax_output : {torch_exp_output}") 
 
-with open("../tensors/gpu_output/gpu_torch_exp_output.pickle", "wb") as fw:
-    pickle.dump(torch_exp_output, fw)
-    
 # softmax
 
 m = nn.Softmax(dim = 2)
 torch_softmax_output = m(torch_input)
 torch_softmax_output = torch_softmax_output.to('cpu')
-print(f"torch_softmax_output : {torch_softmax_output}") 
+#print(f"torch_softmax_output : {torch_softmax_output}") 
 
-with open("../tensors/gpu_output/gpu_torch_softmax_output.pickle", "wb") as fw:
+with open("./tensors/gpu_output/negat10/gpu_torch_softmax_output.pickle", "wb") as fw:
     pickle.dump(torch_softmax_output, fw)
-
+    
 # GeLU
 
 m = nn.GELU()
 torch_GeLU_output = m(torch_input)
 torch_GeLU_output = torch_GeLU_output.to('cpu')
-print(f"torch_GeLU_output : {torch_GeLU_output}") 
+print(f"torch_GeLU_output : {torch_GeLU_output[0][0][0]}") 
 
-with open("../tensors/gpu_output/gpu_torch_GeLU_output.pickle", "wb") as fw:
+with open("./tensors/gpu_output/negat10/gpu_torch_GeLU_output.pickle", "wb") as fw:
     pickle.dump(torch_GeLU_output, fw)
     
 # tanh
 
 torch_tanh_output = torch.tanh(torch_input)
 torch_tanh_output = torch_tanh_output.to('cpu')
-print(f"torch_tanh_output : {torch_tanh_output}") 
+#print(f"torch_tanh_output : {torch_tanh_output}") 
 
-with open("../tensors/gpu_output/gpu_torch_tanh_output.pickle", "wb") as fw:
+with open("./tensors/gpu_output/negat10/gpu_torch_tanh_output.pickle", "wb") as fw:
     pickle.dump(torch_tanh_output, fw)
     
-# sigmoid
 
-torch_sigmoid_output = torch.sigmoid(torch_input)
-torch_sigmoid_output = torch_sigmoid_output.to('cpu')
-print(f"torch_sigmoid_output : {torch_sigmoid_output}")
 
-with open("../tensors/gpu_output/gpu_torch_sigmoid_output.pickle", "wb") as fw:
-    pickle.dump(torch_sigmoid_output, fw)
-    
 # swish(SiLU)
 
 m = nn.SiLU()
 torch_swish_output = m(torch_input)
 torch_swish_output = torch_swish_output.to('cpu')
-print(f"torch_swish_output : {torch_swish_output}") 
+#print(f"torch_swish_output : {torch_swish_output}") 
 
-with open("../tensors/gpu_output/gpu_torch_swish_output.pickle", "wb") as fw:
+with open("./tensors/gpu_output/negat10/gpu_torch_swish_output.pickle", "wb") as fw:
     pickle.dump(torch_swish_output, fw)
     
